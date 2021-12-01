236b621f544a36eef031e5a9966120904004e452
Will Prosser <will.prosser@gmail.com>
2021-12-01 11:09:02 +0000

    Day 1 part 1 solution

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
-    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
     .AddSingleton<IDayApi, DayApi>();
 
 await builder.Build().RunAsync();
--- Services/DayService.cs
+++ Services/DayService.cs
@@ -1,15 +1,31 @@
+
+using Microsoft.AspNetCore.Components;
+
 namespace AdventOfCodeBlazor.Services
 {
     public interface IDayApi
     {
         bool HasCompletedDay(int day);
+        Task<string> GetDayInput(int day);
     }
 
     public class DayApi : IDayApi
     {
+        public DayApi(HttpClient http)
+        {
+            Http = http;
+        }
+
+        [Inject]
+        private HttpClient Http { get; set; }
         private readonly int[] _completedDays = new int[]{
                 1
             };
+
+        public async Task<string> GetDayInput(int day)
+        {
+            return await Http.GetStringAsync($"Input/Day{day}.input");
+        }
 
         public bool HasCompletedDay(int day) => _completedDays.Contains(day);
     }
