c134a7cafcef1659721f60b007331bf481ade4bc
Will Prosser <will.prosser@gmail.com>
2021-12-07 09:19:47 +0000

    Day 7 solutions 1 and 2

--- Services/DayService.cs
+++ Services/DayService.cs
@@ -24,7 +24,8 @@ namespace AdventOfCodeBlazor.Services
                 3,
                 4,
                 5,
-                6
+                6,
+                7,
             };
 
         public async Task<string> GetDayInput(int day)
